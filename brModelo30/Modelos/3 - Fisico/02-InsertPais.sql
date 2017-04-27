5e051743bf855c1e73d9ab8f6cdf20686c38d631
Eduardocoutto <eduardocoouto@gmail.com>
2017-04-27 11:35:34 -0300

    Add files via upload

---
+++ brModelo30/Modelos/3 - Fisico/02-InsertPais.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO pais (ID_pais, nome)
+VALUES (1234567890, 'João'),
+        (1234567890, 'Maria'),
+        (1234567890, 'Pera'),
+        (1234567890, 'Pedro'),
+        (1234567890, 'Bruno');
+        
