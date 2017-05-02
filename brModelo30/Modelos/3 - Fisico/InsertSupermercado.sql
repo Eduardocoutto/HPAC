c30acddd2895a2c4d916273b138998415fbdfde9
Edmiltho <edmiltho@hotmail.com>
2017-05-02 20:40:42 -0300

    Add files via upload

---
+++ brModelo30/Modelos/3 - Fisico/InsertSupermercado.sql
@@ -0,0 +1,11 @@
+INSERT INTO supermercado(ID_supe ,ID_ende ,nome , unidade ,logo)
+VALUES (01, 101, 'Devens', 'Serra', NULL),
+		(02, 102, 'Extrabom', 'Cariacica', NULL),
+		(03, 103, 'Wallmart', 'Vitória', NULL),
+		(04, 104, 'Carone', 'Serra', NULL),
+		(05, 105, 'Meridional', 'Aracruz', NULL),
+		(06, 106, 'Devens', 'Aracruz', NULL),
+		(07, 107, 'Carrefour', 'Serra', NULL),
+		(08, 108, 'Extrabom', 'Vila Velha', NULL),
+		(09, 109, 'Carrefour', 'Vitória', NULL),
+		(010, 200, 'Wallmart', 'Linhares', NULL);
