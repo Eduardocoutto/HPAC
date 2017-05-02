424332978444397fdcec521ebc1f182b9d30a55b
Edmiltho <edmiltho@hotmail.com>
2017-05-02 20:24:34 -0300

    Add files via upload

---
+++ brModelo30/Modelos/3 - Fisico/InsertAvalia.sql
@@ -0,0 +1,6 @@
+INSERT INTO avalia(nota , Atributo2 ,ID_pord , ID_pess)
+VALUES (1, 'Banana podre', '1', '001'),
+		(5, 'Dá pro gasto', '8', '002'),
+		(9, 'Muito bom', '20', '003'),
+		(7, 'Dá pra melhorar', '18', '004'),
+		(8, 'Funcionou', '12', '005');
