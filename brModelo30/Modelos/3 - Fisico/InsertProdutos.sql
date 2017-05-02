edf6b4c0585f51d7749ac0fe1c6ca80ecb48af33
Edmiltho <edmiltho@hotmail.com>
2017-05-02 19:47:05 -0300

    Add files via upload

---
+++ brModelo30/Modelos/3 - Fisico/InsertProdutos.sql
@@ -0,0 +1,23 @@
+INSERT INTO pais (ID_pord,nome ,tamanho ,marca ,tipoMedida, quantidade ,possui_ID_pord )
+VALUES (1, 'Banana', NULL, NULL, 'kg', 1, 1),
+		(2, 'Coca Cola', 300, NULL, 'ml', 1, 1),
+		(3, 'Coca Cola', 600, NULL, 'ml', 1, 1),
+		(4, 'Coca Cola', 2, NUll, 'l', 1, 1),
+		(5, 'Arroz', 1, 'sepó', 'kg', 1, 1),
+		(6, 'Feijao', 2, 'sabugo', 'kg', 1, 1),
+		(7, 'Milho', 1, 'sadiga', 'kg', 1, 1),
+		(8, 'Escova de dente', 10, 'Lonis', 'cm', 1, 1),
+		(9, 'Copos de plastico', 300, 'Nanis', 'ml', 10, 1),
+		(10, 'Pendrive', 3, 'Kingspon', 'cm', 1, 1),
+		(11, 'Barra de chocolate', 200, 'maroto', 'g', 1, 1),
+		(12, 'Lãmpada LED', 6, 'lapda', 'watt', 1, 1),
+		(13, 'Lâmpada Incandescente', 40, 'lapda', 'watt', 1, 1),
+		(14, 'Trigo', 1, 'felizardo', 'kg', 1, 1),
+		(15, 'Biscoito recheado', 100, 'Prakinas', 'g', 1, 1),
+		(16, 'Batata chips', 75, 'Muffles', 'g', 1, 1),
+		(17, 'Detergente', 500, 'Limol', 'ml', 1, 1),
+		(18, 'Par de Chinelos', NULL, 'Cavaibana', NULL, 1, 1),
+		(19, 'Carne picanha', 1, NULL, 'kg', 1, 1),
+		(20, 'Pacote de lenço', NULL, 'glinex', NULL, 1, 1);
+
+		
