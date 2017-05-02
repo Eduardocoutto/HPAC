52b814048bea5959f598dfe0561850debde2d9ba
Eduardocoutto <eduardocoouto@gmail.com>
2017-05-02 08:03:36 -0300

    Update 02-InsertPais.sql

--- brModelo30/Modelos/3 - Fisico/02-InsertPais.sql
+++ brModelo30/Modelos/3 - Fisico/02-InsertPais.sql
@@ -1,8 +1,14 @@
 
 INSERT INTO pais (ID_pais, nome)
-VALUES (1234567890, 'João'),
-        (1234567890, 'Maria'),
-        (1234567890, 'Pera'),
-        (1234567890, 'Pedro'),
-        (1234567890, 'Bruno');
+VALUES (1, 'Brasil'),
+        (2, 'Estados Unidos'),
+        (3, 'França'),
+        (4, 'Itália'),
+        (5, 'Canadá'),
+        (6, 'Chile'),
+        (7, 'Russia'),
+        (8, 'Alemanha'),
+        (9, 'México'),
+        (10, 'Egito'),
+        (11, 'China');
         
