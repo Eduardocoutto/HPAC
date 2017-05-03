9c73729f8e6f3ed5d7028624a1c5e29f59fc0586
Eduardocoutto <eduardocoouto@gmail.com>
2017-05-03 08:17:52 -0300

    Add files via upload

---
+++ brModelo30/Modelos/3 - Fisico/04-InsertCidades.sql
@@ -0,0 +1,16 @@
+--@EduardoCouto
+--Adiciona algumas cidades do Brasil e EUA.
+
+INSERT INTO cidade (ID_cida, ID_esta, nome)
+VALUES (1, 1, 'Serra'),
+		(2, 1, 'Vitória'),
+        (3, 1, 'Vila Velha'),
+        (4, 1, 'Cariacica'),
+        (5, 1, 'Guarapari'),
+        (6, 1, 'Rio de Janeiro'),
+        (7, 7, 'Salvador'),
+        (8, 7, 'Feira de Santana'),
+		(9, 2, 'São Paulo'),
+        (10, 2, 'Campinas'),
+        (11, 6, 'Rio de Janeiro'),
+        (12, 6, 'Niterói');
