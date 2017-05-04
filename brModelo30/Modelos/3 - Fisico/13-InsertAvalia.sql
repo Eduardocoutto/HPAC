6454b5fdb2a57e4f067b3a078c4b2ca3f4bbbca8
Edmiltho <edmiltho@hotmail.com>
2017-05-04 19:15:03 -0300

    adicionando valores

--- brModelo30/Modelos/3 - Fisico/13-InsertAvalia.sql
+++ brModelo30/Modelos/3 - Fisico/13-InsertAvalia.sql
@@ -1,6 +1,6 @@
-INSERT INTO avalia(nota , Atributo2 ,ID_prod , ID_pess)
-VALUES (1, 'Banana podre', '1', '01'),
-	(5, 'Dá pro gasto', '8', '02'),
-	(9, 'Muito bom', '20', '03'),
-	(7, 'Dá pra melhorar', '18', '04'),
-	(8, 'Funcionou', '12', '05');
+INSERT INTO avalia(nota , Atributo2 ,ID_supe, ID_prod , ID_pess)
+VALUES (1, 'Banana podre', 1, 1, 01),
+	(5, 'Dá pro gasto', 8, 2, 02),
+	(9, 'Muito bom', 3, 5, 03),
+	(7, 'Dá pra melhorar', 4, 7, 04),
+	(8, 'Funcionou', 5, 9, 05);
