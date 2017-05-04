e4f7ca9aa81a40f8b138f54c70b03a45d0ca9dff
Edmiltho <edmiltho@hotmail.com>
2017-05-04 18:35:47 -0300

    Update 03-InsertEstados.sql

--- brModelo30/Modelos/3 - Fisico/03-InsertEstados.sql
+++ brModelo30/Modelos/3 - Fisico/03-InsertEstados.sql
@@ -3,14 +3,14 @@
 
 INSERT INTO estado (ID_pais, ID_esta, nome)
 VALUES (1, 1, 'Espirito Santo'),
-		(1, 2, 'São Paulo'),
+	(1, 2, 'São Paulo'),
         (1, 3, 'Goias'),
         (1, 4, 'Amazonas'),
         (1, 5, 'Mato Grosso do Sul'),
         (1, 6, 'Rio de Janeiro'),
         (1, 7, 'Bahia'),
         (1, 8, 'Rio de Janeiro'),
-		(2, 9, 'Texas'),
+	(2, 9, 'Texas'),
         (2, 10, 'Califórnia'),
         (2, 11, 'Flórida'),
         (2, 12, 'Alasca');
