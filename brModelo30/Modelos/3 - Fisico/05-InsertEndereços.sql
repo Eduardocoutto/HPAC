e2d0a7e4c88fc7e9bd7a97e2fdce41802702b144
Edmiltho <edmiltho@hotmail.com>
2017-05-04 18:36:50 -0300

    Update 05-InsertEndereços.sql

--- brModelo30/Modelos/3 - Fisico/05-InsertEndereços.sql
+++ brModelo30/Modelos/3 - Fisico/05-InsertEndereços.sql
@@ -1,7 +1,7 @@
 --@EduardoCouto
 --Adiciona endereços
 
-INSERT INTO endereço (id_ende, ID_cida, bairro, cep, logradouro, nº, complemento)
+INSERT INTO endereço (id_ende, ID_cida, bairro, cep, logradouro, numero, complemento)
 VALUES (1,1, 'Manguinhos', '29123123','Rua dos estudantes',661,'Ifes Serra'),
         (2,1, 'Laranjeiras', '29165000', 'Av Central', 152, 'Comercio'),
         (3,2, 'Goiabeiras', '29165001', 'Av Fernando Ferrari', 122, 'Nenhum'),
