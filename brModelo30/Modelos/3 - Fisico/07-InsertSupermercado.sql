499ee460eff99378f28a1bfae227c74cad7dbe08
Igor Caniçali <Igor Caniali>
2017-05-05 20:24:34 -0300

    identação

--- brModelo30/Modelos/3 - Fisico/07-InsertSupermercado.sql
+++ brModelo30/Modelos/3 - Fisico/07-InsertSupermercado.sql
@@ -1,11 +1,11 @@
 INSERT INTO supermecado(ID_supe ,ID_ende ,nome , unidade ,logo)
-VALUES (1, 1, 'Devens', 'Serra', NULL),
-	(2, 2, 'Extrabom', 'Cariacica', NULL),
-	(3, 3, 'Wallmart', 'Vitória', NULL),
-	(4, 4, 'Carone', 'Serra', NULL),
-	(5, 5, 'Meridional', 'Aracruz', NULL),
-	(6, 1, 'Devens', 'Aracruz', NULL),
-	(7, 2, 'Carrefour', 'Serra', NULL),
-	(8, 3, 'Extrabom', 'Vila Velha', NULL),
-	(9, 4, 'Carrefour', 'Vitória', NULL),
-	(10, 5, 'Wallmart', 'Linhares', NULL);
+VALUES (1,  1, 'Devens',     'Serra',      NULL),
+	   (2,  2, 'Extrabom',   'Cariacica',  NULL),
+	   (3,  3, 'Wallmart',   'Vitória',    NULL),
+	   (4,  4, 'Carone',     'Serra',      NULL),
+	   (5,  5, 'Meridional', 'Aracruz',    NULL),
+	   (6,  1, 'Devens',     'Aracruz',    NULL),
+	   (7,  2, 'Carrefour',  'Serra',      NULL),
+	   (8,  3, 'Extrabom',   'Vila Velha', NULL),
+	   (9,  4, 'Carrefour',  'Vitória',    NULL),
+	   (10, 5, 'Wallmart',   'Linhares',   NULL);
