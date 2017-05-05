3cc843730a5937b066bab2b7fd748dd227bdacb7
IgorCanicali <IgorCanicali@users.noreply.github.com>
2017-05-05 20:32:47 -0300

    reidentando pela n+1 vez

--- brModelo30/Modelos/3 - Fisico/01-Script_InsertTabelas.sql
+++ brModelo30/Modelos/3 - Fisico/01-Script_InsertTabelas.sql
@@ -40,11 +40,11 @@ VALUES  (1,  1, 'Serra'           ),
         (12, 6, 'Niterói'         );
 		
 INSERT INTO endereço (id_ende, ID_cida, bairro, cep, logradouro, numero, complemento)
-VALUES  (1, 1, 'Manguinhos',  		'29123123', 'Rua dos estudantes',  661, 'Ifes Serra'),
-        (2, 1, 'Laranjeiras', 		'29165000', 'Av Central', 		   152, 'Comercio'	),
-        (3, 2, 'Goiabeiras',  		'29165001', 'Av Fernando Ferrari', 122, 'Nenhum'	),
-        (4, 3, 'Jardim Marilandia', '29179450', 'Av Teste', 		   14,  'Comercio'	),
-        (5, 1, 'Barcelona', 		'29666200', 'Rua Aquario Santos',  433, 'Nope'		);
+VALUES  (1, 1, 'Manguinhos',  	    '29123123', 'Rua dos estudantes',  661, 'Ifes Serra'),
+        (2, 1, 'Laranjeiras', 	    '29165000', 'Av Central', 	       152, 'Comercio'	),
+        (3, 2, 'Goiabeiras',  	    '29165001', 'Av Fernando Ferrari', 122, 'Nenhum'	),
+        (4, 3, 'Jardim Marilandia', '29179450', 'Av Teste', 	       14,  'Comercio'	),
+        (5, 1, 'Barcelona', 	    '29666200', 'Rua Aquario Santos',  433, 'Nope' 	);
 		
 INSERT INTO avaliacao_sistema (nota , comentario)
 VALUES  (1, 'Banana podre'   ),
