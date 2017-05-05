e69d40c56ff69e8a252a9d65f67630508ce8e7f3
Igor Caniçali <Igor Caniali>
2017-05-04 22:17:45 -0300

    ajeitando identação

--- brModelo30/Modelos/3 - Fisico/02-InsertPais.sql
+++ brModelo30/Modelos/3 - Fisico/02-InsertPais.sql
@@ -1,14 +1,14 @@
 
 INSERT INTO pais (ID_pais, nome)
-VALUES (1, 'Brasil'),
-        (2, 'Estados Unidos'),
-        (3, 'França'),
-        (4, 'Itália'),
-        (5, 'Canadá'),
-        (6, 'Chile'),
-        (7, 'Russia'),
-        (8, 'Alemanha'),
-        (9, 'México'),
-        (10, 'Egito'),
-        (11, 'China');
+VALUES (1,  'Brasil'        ),
+       (2,  'Estados Unidos'),
+       (3,  'França'        ),
+       (4,  'Itália'        ),
+       (5,  'Canadá'        ),
+       (6,  'Chile'         ),
+       (7,  'Russia'        ),
+       (8,  'Alemanha'      ),
+       (9,  'México'        ),
+       (10, 'Egito'         ),
+       (11, 'China'         );
         
--- brModelo30/Modelos/3 - Fisico/03-InsertEstados.sql
+++ brModelo30/Modelos/3 - Fisico/03-InsertEstados.sql
@@ -2,15 +2,15 @@
 --Adiciona alguns estados do Brasil e EUA.
 
 INSERT INTO estado (ID_pais, ID_esta, nome)
-VALUES (1, 1, 'Espirito Santo'),
-	(1, 2, 'São Paulo'),
-        (1, 3, 'Goias'),
-        (1, 4, 'Amazonas'),
-        (1, 5, 'Mato Grosso do Sul'),
-        (1, 6, 'Rio de Janeiro'),
-        (1, 7, 'Bahia'),
-        (1, 8, 'Rio de Janeiro'),
-	(2, 9, 'Texas'),
-        (2, 10, 'Califórnia'),
-        (2, 11, 'Flórida'),
-        (2, 12, 'Alasca');
+VALUES (1, 1,  'Espirito Santo'    ),
+       (1, 2,  'São Paulo'         ),
+       (1, 3,  'Goias'             ),
+       (1, 4,  'Amazonas'          ),
+       (1, 5,  'Mato Grosso do Sul'),
+       (1, 6,  'Rio de Janeiro'    ),
+       (1, 7,  'Bahia'             ),
+       (1, 8,  'Rio de Janeiro'    ),
+       (2, 9,  'Texas'             ),
+       (2, 10, 'Califórnia'        ),
+       (2, 11, 'Flórida'           ),
+       (2, 12, 'Alasca'            );
