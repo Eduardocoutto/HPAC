65901e49e8fa1afdf78a8e6346e719edcea25ad3
Eduardocoutto <eduardocoouto@gmail.com>
2017-05-11 10:54:45 -0300

    Update 01-Script_InsertTabelas.sql

--- brModelo30/Modelos/3 - Fisico/01-Script_InsertTabelas.sql
+++ brModelo30/Modelos/3 - Fisico/01-Script_InsertTabelas.sql
@@ -44,7 +44,7 @@ VALUES  (1, 1, 'Manguinhos',  	    '29123123', 'Rua dos estudantes',  661, 'Ifes
         (2, 1, 'Laranjeiras', 	    '29165000', 'Av Central', 	       152, 'Comercio'	),
         (3, 2, 'Goiabeiras',  	    '29165001', 'Av Fernando Ferrari', 122, 'Nenhum'	),
         (4, 3, 'Jardim Marilandia', '29179450', 'Av Teste', 	       14,  'Comercio'	),
-        (5, 1, 'Barcelona', 	    '29666200', 'Rua Aquario Santos',  433, 'Nope' 	)
+        (5, 1, 'Barcelona', 	    '29666200', 'Rua Aquario Santos',  433, 'Nope' 	),
         (6, 1, 'Feu Rosa',          '29165666', 'Rua teste',           11,  'Nenhum'),
         (7, 2, 'São Pedro',         '29887887', 'Rua Absurda',         12,  'Ifes Vitoria'),
         (8, 2, 'São Torquato',      '29665123', 'Rua Terminal',        22,  'Certurb'),
