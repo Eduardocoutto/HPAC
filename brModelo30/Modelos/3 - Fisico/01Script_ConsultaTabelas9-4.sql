a5c277ff44dc670327a786a4f0d7f97224b4fc70
Eduardocoutto <eduardocoouto@gmail.com>
2017-06-06 19:42:21 -0300

    Add files via upload

---
+++ brModelo30/Modelos/3 - Fisico/01Script_ConsultaTabelas9-4.sql
@@ -0,0 +1,8 @@
+
+SELECT * FROM PRODUTO WHERE nome LIKE '%Carne%';
+SELECT  comentario FROM avalia_sys WHERE comentario LIKE '%bom%';
+SELECT  * FROM supermercado where nome like '%mart%' and unidade like 'V%';
+SELECT  * FROM supermercado where nome like '__ro__';
+SELECT  * FROM lista_de_compras as LC WHERE LC.nome like '%ca_a%';
+
+
